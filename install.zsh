7ac4d1dd720d0dd9f2844743de1971540013334e
Nick Hale <njohnhale@gmail.com>
2022-07-20 11:02:18 -0400

    add special handling to file linking

--- install.zsh
+++ install.zsh
@@ -1,22 +1,34 @@
-#!/bin/zsh
-
+#!/bin/env zsh
 set -e
 setopt EXTENDED_GLOB
 
+# Find the current directory
+local DOTFILES_DIR="$(cd $(dirname ${BASH_SOURCE[0]-$0}) && pwd)"
+
 # Default config variables 
-export INSTALL_DIR=${INSTALL_DIR:-$HOME}
-export DOTFILES_DIR=${DOTFILES_DIR:-$HOME/dotfiles}
-export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$INSTALL_DIR/.config}
+local XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+local INSTALL_DIR="${INSTALL_DIR:-$HOME}"
 
 # Symlink dotfiles
+declare -A special
+special[kitty.conf]="${XDG_CONFIG_HOME}/kitty/kitty.conf"
+special[Brewfile]='SKIP'
+special[README.md]='SKIP'
+special[monokai-soda.itermcolors]='SKIP'
+special[.git]='SKIP'
+
 echo "Linking dotfiles..."
-for FILE in $DOTFILES_DIR/(.??*)(.N); do;
-    local TARGET=$INSTALL_DIR/${FILE:t}
-    if [[ ! -a $TARGET ]]; then
-        print -P -- "   %F{002}Linking file:%f $FILE => $TARGET"
+for FILE in $DOTFILES_DIR/*(D); do;
+    local TARGET="${special[${FILE:t}]-${INSTALL_DIR}/${FILE:t}}"
+
+    if [[ $TARGET == 'SKIP' ]]; then
+        print -P -- "   %F{014}Link explicitly skipped:%f $FILE"
+    elif [[ ! -a $TARGET ]]; then
+        print -P -- "   %F{002}Linking:%f $FILE => $TARGET"
         ln -s $FILE $TARGET
     else
         print -P -- "   %F{011}Link already exists:%f $FILE => $TARGET"
     fi
 done
+
 
